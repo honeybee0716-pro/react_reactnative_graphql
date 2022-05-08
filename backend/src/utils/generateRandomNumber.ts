1e80187bea43926e05da4629eb0e9c7c3cffc1b5
Joey Fenny <16143306+JoeyFenny@users.noreply.github.com>
2022-05-08 14:03:11 -0500

    uuid and random number added

---
+++ backend/src/utils/generateRandomNumber.ts
@@ -0,0 +1,7 @@
+export const generatorRandomSixDigitNumber = () => {
+  const max = 999999;
+  const min = 100000;
+  const randomNumber = Math.trunc(Math.random() * (max - min) + min);
+
+  return randomNumber;
+};
