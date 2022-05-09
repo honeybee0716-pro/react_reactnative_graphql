d6c31f8df466ab0bd3f68743a7667573bce1d26c
Joey Fenny <16143306+JoeyFenny@users.noreply.github.com>
2022-05-08 22:42:19 -0500

    test

--- frontend/shared/screens/Landing/index.tsx
+++ frontend/shared/screens/Landing/index.tsx
@@ -1,15 +1,1 @@
-import { Navbar } from '../../components/Navbar'
-import { Feed } from '../../components/Feed'
-import * as Styled from './styles'
-import { MainGutter } from '../../components/MainGutter/styles'
-
-export const Landing = () => {
-  return (
-    <Styled.Container>
-      <MainGutter>
-        <Navbar />
-        <Feed />
-      </MainGutter>
-    </Styled.Container>
-  )
-}
+export const Landing = () => null;
