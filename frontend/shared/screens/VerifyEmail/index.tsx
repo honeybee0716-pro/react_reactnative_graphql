865068be5af9ceffac78279f2d44f8db201253d7
Joey Fenny <16143306+JoeyFenny@users.noreply.github.com>
2022-05-08 22:39:58 -0500

    test

--- frontend/shared/screens/VerifyEmail/index.tsx
+++ frontend/shared/screens/VerifyEmail/index.tsx
@@ -1,17 +1,1 @@
-import { Text } from '../../styles/convertedComponents'
-import { Navbar } from '../../components/Navbar'
-import { Feed } from '../../components/Feed'
-import * as Styled from './styles'
-import { MainGutter } from '../../components/MainGutter/styles'
-
-export const HomeScreen = () => {
-  return (
-    <Styled.Container>
-      <MainGutter>
-        <Navbar />
-        <Feed />
-        <Text>Home</Text>
-      </MainGutter>
-    </Styled.Container>
-  )
-}
+export const VerifyEmailScreen = () => null;
