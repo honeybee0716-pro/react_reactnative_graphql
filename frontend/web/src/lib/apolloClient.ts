48c7b495f47bc85be62b5836b4e295b2d9224c9f
Joey Fenny <joeyfenny@gmail.com>
2022-11-07 13:39:50 -0500

    Miscellaneous code clean up

--- frontend/web/src/lib/apolloClient.ts
+++ frontend/web/src/lib/apolloClient.ts
@@ -2,11 +2,6 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
 import { onError } from '@apollo/client/link/error'
 import { setContext } from '@apollo/client/link/context'
 
-console.log(
-  'process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT',
-  process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
-)
-
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
 })
