40ec83a373a4e981a0b3328216fa0970d46ec041
Muhammad Hamza Latif <85412839+naqshbandhamza@users.noreply.github.com>
2022-11-13 14:03:13 +0500

    Add files via upload

---
+++ frontend/shared/screens/Account/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  Box,
+  Center,
+  Hidden,
+  Text,
+  Image,
+  HStack,
+  VStack,
+  Checkbox,
+  Pressable
+} from 'native-base'
+import { theme } from 'shared/styles/theme'
+import { Fragment } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper'
+import DashboardLayout from 'shared/layouts/DashboardLayout'
+import IconCornerUpRight from 'shared/components/icons/IconCornerUpRight'
+import IconDollarSign from 'shared/components/icons/IconDollarSign'
+import IconDownload from 'shared/components/icons/IconDownload'
+import IconMoreVertical from 'shared/components/icons/IconMoreVertical'
+import IconChevronDown from 'shared/components/icons/IconChevronDown'
+import IconCreditCard from 'shared/components/icons/IconCreditCard'
+import IconLightcoin from 'shared/components/icons/IconLightcoin'
+import IconNFC from 'shared/components/icons/IconNFC'
+import IconEdit from 'shared/components/icons/IconEdit'
+import IconMasterCard from 'shared/components/icons/IconMasterCard'
+import IconVISA from 'shared/components/icons/IconVISA'
+import IconHome from 'shared/components/icons/IconHome'
+import IconGlobe from 'shared/components/icons/IconGlobe'
+import IconPlus from 'shared/components/icons/IconPlus'
+
+export default function Account() {
+  return (
+    <>
+      <h1>account page</h1>
+    </>
+  )
+}
